c00df934ad3327370c79fee3a752a8ca666dbca5
nyambura00 <winnienyambura14@gmail.com>
2021-02-25 16:17:09 +0300

    worked on the Homepage TopNav

---
+++ Components/TopNav.js
@@ -0,0 +1,32 @@
+import styles from '../styles/topnav.module.css'
+import React from 'react'
+import Image from 'next/image'
+
+const TopNav = () => {
+    return(
+        <nav class="flex flex-row space-x-7 m-2 border-b-2">
+            <Image
+                class="m-8"
+                src="/writing.png"
+                alt="Premium logo"
+                width={150}
+                height={110}
+                />
+            <ul>
+                <div class="flex flex-row space-x-10 fixed top-10 left-50">
+                <li className="rounded hover:bg-red-700 text-2xl">Home</li>
+                <li className="rounded hover:bg-red-700 text-2xl">Services</li>
+                <li className="rounded hover:bg-red-700 text-2xl">About us</li>
+                <li className="rounded hover:bg-red-700 text-2xl">Pricing</li>
+                <li className="rounded hover:bg-red-700 text-2xl">Contact Us</li>
+                </div>
+                <div className="flex flex-row space-x-5 fixed top-10 right-5" class="absolute top-10 right-0">
+                <button className="rounded bg-red-700 text-2xl hover:underline">Log In</button>
+                <button className="rounded bg-red-700 text-2xl hover:underline">Sign Up</button>
+                </div>
+            </ul>
+        </nav>
+    )
+}
+
+export default TopNav
---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+import TopNav from '../Components/TopNav'
+
+const Homepage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Premium workplace!</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Online Writing, Get work Done, Get remote work done" />
+        <meta name="keywords" content="Writing, Online work, Post Work, Assignments" />
+      </Head>
+
+      <TopNav />
+
+      <main>
+        
+      </main>
+
+    </div>
+  )
+}
+
+export default Homepage
