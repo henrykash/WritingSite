7ce71dff0ac9ae418ab394f17a3c89076b778713
Mogakamo <mogaka.amo254@gmail.com>
2021-03-16 16:25:52 +0300

    Commit

---
+++ Components/Contact.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class Contact extends React.Component {
+    render() {
+        return (
+            <div className="section">
+                <div className="container">
+                    
+                </div>
+            </div>
+        )
+    }
+}
