8547bcfbc127f7e78d1a544cadb4accfdf188164
Mogakamo <mogaka.amo254@gmail.com>
2021-03-19 13:36:15 +0300

    Commit

---
+++ utils/db/index.js
@@ -0,0 +1,13 @@
+import admin from 'firebase-admin'
+import serviceAccount from './serviceAccountKey.json'
+
+if (!admin.apps.length) {
+    try{
+        adimin.initializeApp({
+            credentials: admin.credentials.cert(serviceAccount),
+            databaseURL: ""
+        })
+    } catch (e) {
+        console.log('Firebase admin initialization error', error.stack);
+    }
+}
