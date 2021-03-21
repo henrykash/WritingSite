d59406ed14e400cd7a2ba0ad446d51bc9d5bc46c
Mogakamo <mogaka.amo254@gmail.com>
2021-03-21 14:53:24 +0300

    Commit

--- pages/signup.js
+++ pages/signup.js
@@ -1,1 +1,20 @@
-import Link f
+import Link from 'next/link'
+import React from 'react'
+
+
+const SignUpPage = () => {
+    return (
+        <div className="min-h-screen flex bg-gray-200">
+            <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                <div className="text-center mt-24">
+                    <h2 className="mt-6 text-center text-3xl leading-9 font-extrabold text-gray-900">Sign up</h2>
+                    <p className="mt-2 text-center text-md text-gray-600">already have an account?{' '}
+                    <Link href="/login">
+                        <a href="#" className="text-blue-500">Log in</a>
+                    </Link>
+                    </p>
+                </div>    
+            </div>
+        </div>
+    )
+}
