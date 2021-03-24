fb7cabe21406c91bf234e0431603408f30f87006
nyambura00 <winnienyambura14@gmail.com>
2021-03-24 10:20:23 +0300

    working on auth

--- auth.js
+++ auth.js
@@ -4,3 +4,14 @@ import nookies from 'nookies';
 import firebaseClient from './firebaseClient'
 import firebase from 'firebase/app'
 import 'firebase/auth'
+
+const AuthContext = createContext({});
+
+export const AuthProvider = ({children}) => {
+    firebaseClient();
+    const [user, setUser] = useState(null);
+
+    useEffect(() => {
+        
+    })
+}
