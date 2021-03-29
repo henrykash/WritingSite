1f0ce6212af64849aee3e719347eb7a78f0672c9
nyambura00 <winnienyambura14@gmail.com>
2021-03-29 16:14:58 +0300

    firebase auth

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/index.css'
 import 'tailwindcss/tailwind.css'
+import initAuth from '../pages/initAuth'
 
+
+initAuth()
 function MyApp({ Component, pageProps }) {
   return (
   <div>
--- pages/initAuth.js
+++ pages/initAuth.js
@@ -1,6 +1,10 @@
-/*import { init } from 'next-firebase-auth'
+import { init } from 'next-firebase-auth'
+//Creating a module to initialize next-firebase-auth
 
 const initAuth = () => {
+  var admin = require("firebase-admin");
+  var serviceAccount = require("/serviceAccount.json");
+
   init({
     authPageURL: '/auth',
     appPageURL: '/',
@@ -8,22 +12,17 @@ const initAuth = () => {
     logoutAPIEndpoint: '/api/logout', // required
     // Required in most cases.
     firebaseAdminInitConfig: {
-      credential: {
-        projectId: 'my-example-app-id',
-        clientEmail: 'example-abc123@my-example-app.iam.gserviceaccount.com',
-        // The private key must not be accesssible on the client side.
-        privateKey: process.env.FIREBASE_PRIVATE_KEY,
-      },
-      databaseURL: 'https://my-example-app.firebaseio.com',
+      credential: admin.credential.cert(serviceAccount),
+      databaseURL: "https://writing-site-3853f-default-rtdb.firebaseio.com",
     },
     firebaseClientInitConfig: {
-      apiKey: 'MyExampleAppAPIKey123', // required
-      authDomain: 'my-example-app.firebaseapp.com',
-      databaseURL: 'https://my-example-app.firebaseio.com',
-      projectId: 'my-example-app-id',
+      apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY, // required
+      authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+      databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+      projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
     },
     cookies: {
-      name: 'ExampleApp', // required
+      name: 'Get Work Done', // required
       // Keys are required unless you set `signed` to `false`.
       // The keys cannot be accessible on the client side.
       keys: [
@@ -41,4 +40,4 @@ const initAuth = () => {
   })
 }
 
-export default initAuth*/
+export default initAuth
