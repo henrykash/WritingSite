7e477e589e2fc8d3cf6f5d81cea00a7ec931d926
nyambura00 <winnienyambura14@gmail.com>
2021-03-31 17:29:01 +0300

    edited the svg logo

--- Components/svg-logo.js
+++ Components/svg-logo.js
@@ -1,5 +1,4 @@
-import CheckCircle from '../assets/svg/check-circle.svg';
-//import XCircle from 'assets/svg/check-circle.svg';
+import CheckCircle from '..public/check-circle.svg';
 
 export default () => {
   <CheckCircle />
