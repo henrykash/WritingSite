b07f3971c2e38b456053a8cd18fb53ec283289bb
Mogakamo <mogaka.amo254@gmail.com>
2021-04-22 17:59:05 +0300

    added the website content

---
+++ .history/Components/about/AboutUs_20210422175830.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import AboutInfo from '../../public/about.json'
+
+ const AboutUs = () => {
+  return (
+    
+  )
+}
+
+export default AboutUs
---
+++ .history/Components/about/AboutUs_20210422175833.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import AboutInfo from '../../public/about.json'
+
+ const AboutUs = () => {
+  return (
+    <>
+
+    <>
+  )
+}
+
+export default AboutUs
