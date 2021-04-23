40647f6a6eb6025ecae07f49e8748fdaa11cb645
Mogakamo <mogaka.amo254@gmail.com>
2021-04-23 09:29:16 +0300

    refreshed my .git files since they're corrupted

--- .history/Components/nav/TopNav_20210423091947.js
+++ .history/Components/nav/TopNav_20210423091947.js
@@ -4,7 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 
 const TopNav = () => {
-<
+<<<<<<< 
   return (
     <>
       <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
