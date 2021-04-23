599fc4299f65e48ad929cea287726d12aa1d91c4
Mogakamo <mogaka.amo254@gmail.com>
2021-04-23 09:39:00 +0300

    created a new top nav

---
+++ .history/Components/nav/TopNav_20210423093713.js
@@ -0,0 +1,9 @@
+import styles from "../../styles/topnav.module.css";
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const TopNav = () => {
+ );
+};
+export default TopNav;
