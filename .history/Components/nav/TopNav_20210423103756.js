b72e83942c18f0f3277b18d6a3e421321d8eb90d
Mogakamo <mogaka.amo254@gmail.com>
2021-04-23 10:39:55 +0300

    commit

--- .history/Components/nav/TopNav_20210423103756.js
+++ .history/Components/nav/TopNav_20210423103756.js
@@ -45,7 +45,9 @@ const TopNav = () => {
               </Link>
             </li>
             <li>
-              <Link href="/signup" class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+              <Link
+    href="/signup"
+                class="inline-flex items-center justify-center h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
               >
                 Sign up
               </Link>
