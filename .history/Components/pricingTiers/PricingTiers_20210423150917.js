2c8dd830b3c03bc6e8dd5e6279660b51cfc79e9d
Mogakamo <mogaka.amo254@gmail.com>
2021-04-23 15:11:32 +0300

    commit

--- .history/Components/pricingTiers/PricingTiers_20210423150917.js
+++ .history/Components/pricingTiers/PricingTiers_20210423150917.js
@@ -160,8 +160,14 @@ const PricingTiers = () => {
                       <p class="font-medium text-gray-300">SSL Certificates</p>
                     </li>
                   </ul>
-                  
-          
+                  <Link href="/checkout">
+                  <button
+                    type="submit"
+                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  >
+                    Get Now
+                  </button>
+                  <Link>
                 </div>
                 <div class="w-11/12 h-2 mx-auto bg-gray-900 rounded-b opacity-75" />
                 <div class="w-10/12 h-2 mx-auto bg-gray-900 rounded-b opacity-50" />
@@ -280,12 +286,15 @@ const PricingTiers = () => {
                       <p class="font-medium text-gray-300">SSL Certificates</p>
                     </li>
                   </ul>
+                  <Link href="/checkout">
                   <button
                     type="submit"
                     class="inline-flex items-center justify-center w-full h-12 px-6 font-semibold tracking-wide text-teal-900 transition duration-200 rounded shadow-md bg-teal-accent-400 hover:bg-teal-accent-700 focus:shadow-outline focus:outline-none"
                   >
                     Get Now
                   </button>
+                  <Link
+                >
                 </div>
                 <div class="w-11/12 h-2 mx-auto bg-gray-900 rounded-b opacity-75" />
                 <div class="w-10/12 h-2 mx-auto bg-gray-900 rounded-b opacity-50" />
--- .history/Components/pricingTiers/PricingTiers_20210423150923.js
+++ .history/Components/pricingTiers/PricingTiers_20210423150923.js
@@ -160,8 +160,13 @@ const PricingTiers = () => {
                       <p class="font-medium text-gray-300">SSL Certificates</p>
                     </li>
                   </ul>
-                  
-          
+                  <Link href="/checko">
+                  <button
+                    type="submit"
+                    class="inline-flex items-center justify-center w-full h-12 px-6 font-medium tracking-wide text-white transition duration-200 rounded shadow-md bg-deep-purple-accent-400 hover:bg-deep-purple-accent-700 focus:shadow-outline focus:outline-none"
+                  >
+                    Get Now
+                  </button>
                 </div>
                 <div class="w-11/12 h-2 mx-auto bg-gray-900 rounded-b opacity-75" />
                 <div class="w-10/12 h-2 mx-auto bg-gray-900 rounded-b opacity-50" />
