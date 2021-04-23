e4bc4977cd125ec85a595fa6822697570a650d92
Mogakamo <mogaka.amo254@gmail.com>
2021-04-23 10:05:21 +0300

    commit

--- .history/Components/nav/TopNav_20210423100415.js
+++ .history/Components/nav/TopNav_20210423100415.js
@@ -8,7 +8,7 @@ const TopNav = () => {
 
   return (
     <>
-      <div class="sticky top-0 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg-maroon-500">
+      <div class="sticky top-0 px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 bg:">
         <div class="relative flex items-center justify-between">
           <a
             href="/"
