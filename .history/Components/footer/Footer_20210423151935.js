3bc28110f3914bb1fb43d4230245ea1c47d4fec0
Mogakamo <mogaka.amo254@gmail.com>
2021-04-23 15:22:26 +0300

    Footer

--- .history/Components/footer/Footer_20210423151935.js
+++ .history/Components/footer/Footer_20210423151935.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Footer = () => {
+export const Footer = () => {
   return (
     <div className="bg-gray-900">
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
@@ -236,5 +236,4 @@ const Footer = () => {
     </div>
   );
 };
-
 export default Footer
--- .history/Components/footer/Footer_20210423151949.js
+++ .history/Components/footer/Footer_20210423151949.js
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-const Footer = () => {
+ex const Footer = () => {
   return (
     <div className="bg-gray-900">
       <div className="px-4 pt-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
