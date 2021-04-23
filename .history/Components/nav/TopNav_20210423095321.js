e2e831c029bdfdc1615c29c1810a542ee4ce9e9e
Mogakamo <mogaka.amo254@gmail.com>
2021-04-23 09:57:01 +0300

    🐛 BUG: solved the topnav error

--- .history/Components/nav/TopNav_20210423095321.js
+++ .history/Components/nav/TopNav_20210423095321.js
@@ -1,11 +1,11 @@
 import styles from "../../styles/topnav.module.css";
-import React, { useState } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
 
 const TopNav = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-
+  
   return (
     <>
       <div class="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
