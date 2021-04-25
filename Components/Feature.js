c4d99c4a7de470de5357dd35d147b4b1ac9e1548
nyambura00 <winnienyambura14@gmail.com>
2021-04-25 20:04:20 +0300

    delete the header

--- Components/Feature.js
+++ Components/Feature.js
@@ -1,4 +1,5 @@
 //Feature section of the homepage
+const { default: TypingComponent } = require("./typing/Typing")
 
 const Features = () => {
   return (
@@ -6,31 +7,8 @@ const Features = () => {
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
           <h1 className="sticky top-0 max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            <span className="relative inline-block">
-              <svg
-                viewBox="0 0 52 24"
-                fill="blue"
-                className="absolute top-0 left-0 z-0 hidden w-32 -mt-8 -ml-20 text-blue-gray-100 lg:w-32 lg:-ml-28 lg:-mt-10 sm:block"
-              >
-                <defs>
-                  <pattern
-                    id="fdca20a0-aeb4-4caf-ba1b-4351eee42363"
-                    x="0"
-                    y="0"
-                    width=".135"
-                    height=".30"
-                  >
-                    <circle cx="1" cy="1" r=".7" />
-                  </pattern>
-                </defs>
-                <rect
-                  fill="url(#fdca20a0-aeb4-4caf-ba1b-4351eee42363)"
-                  width="52"
-                  height="24"
-                />
-              </svg>
-            </span>{" "}
-            Features
+            Get Work Done {''}
+            <TypingComponent />
           </h1>
         </div>
         <div className="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
