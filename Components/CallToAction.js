d885bd857288e4de1c813e9325eaa7abd12c4d79
nyambura00 <winnienyambura14@gmail.com>
2021-04-25 22:20:20 +0300

    on track

--- Components/CallToAction.js
+++ Components/CallToAction.js
@@ -8,7 +8,7 @@ const CallToAction = () => {
             <div className="container px-5 py-24 mx-auto flex items-center md:flex-row flex-col">
               <div className="flex flex-col md:pr-10 md:mb-0 mb-6 pr-0 w-full md:w-auto md:text-left text-center">
                 <h2 className="text-xs text-green-500 tracking-widest font-medium title-font mb-1">Coming soon</h2>
-                <h1 className="md:text-3xl text-2xl font-medium title-font text-gray-900">Get our App from store</h1>
+                <h1 className="md:text-3xl text-2xl font-medium title-font text-gray-900">Download our App</h1>
               </div>
               <div className="flex md:ml-auto md:mr-0 mx-auto items-center flex-shrink-0 space-x-4">
                 <button className="bg-gray-100 inline-flex py-3 px-5 rounded-lg items-center hover:bg-gray-200 focus:outline-none">
