c5394e0c77aa802b36b373f79defefc7cf23d759
nyambura00 <winnienyambura14@gmail.com>
2021-04-25 22:17:29 +0300

    Unsuccessfully added the hidden overflow-x

--- pages/index.js
+++ pages/index.js
@@ -20,7 +20,7 @@ const Homepage = () => {
 
   return (
     <>
-      <div className='overflow-y-hidden'>
+      <div className=''>
         <Head>
           <title>Premium workplace!</title>
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
