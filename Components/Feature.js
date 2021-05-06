285b554e1ee8bb2657446e2bacd9815bb5a7b4e0
nyambura00 <winnienyambura14@gmail.com>
2021-05-06 16:34:36 +0300

    Update Features component

--- Components/Feature.js
+++ Components/Feature.js
@@ -7,8 +7,7 @@ const Features = () => {
       <div className="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div className="max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12">
           <h1 className="sticky top-0 max-w-lg mb-6 font-sans text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
-            Get Work Done {""}
-            <TypingComponent />
+            Our Features
           </h1>
         </div>
         <div className="grid max-w-md gap-8 row-gap-10 sm:mx-auto lg:max-w-full lg:grid-cols-3">
