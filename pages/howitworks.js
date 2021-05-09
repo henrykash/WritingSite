c19ca60f171071e1475c51df327b7aba82724f33
nyambura00 <winnienyambura14@gmail.com>
2021-05-09 08:28:29 +0300

    Update Header

---
+++ pages/howitworks.js
@@ -0,0 +1,15 @@
+import TopNav from '../Components/nav/TopNav'
+import Footer from '../Components/footer/Footer'
+import Steps from '../Components/Steps'
+
+ const howitworks = () => {
+    return (
+        <div>
+            <TopNav />
+            <Steps />
+            <Footer />
+        </div>
+    )
+}
+
+export default howitworks
