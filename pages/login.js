2a09e9d3294cd4c1aea958dc3ef3a7cd696ef8ed
nyambura00 <winnienyambura14@gmail.com>
2021-05-09 08:53:02 +0300

    "working on log in"

--- pages/login.js
+++ pages/login.js
@@ -1,77 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useUser } from '../lib/hooks';
-import TopNav from '../Components/nav/TopNav'
+import Footer from '../Components/footer/Footer'
 
-const LoginPage = () => {
-  const router = useRouter();
-  const [errorMsg, setErrorMsg] = useState('');
-  const [user, { mutate }] = useUser();
-  useEffect(() => {
-    // redirect to home if user is authenticated
-    if (user) router.replace('/');
-  }, [user]);
-
-  async function onSubmit(e) {
-    e.preventDefault();
-    const body = {
-      email: e.currentTarget.email.value,
-      password: e.currentTarget.password.value,
-    };
-    const res = await fetch('/api/auth', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify(body),
-    });
-    if (res.status === 200) {
-      const userObj = await res.json();
-      mutate(userObj);
-    } else {
-      setErrorMsg('Incorrect username or password. Try again!');
-    }
-  }
-
+const login = () => {
   return (
-    <>
-      <TopNav />
-      <Head>
-        <title>Sign in</title>
-      </Head>
-      <h2>Sign in</h2>
-      <form onSubmit={onSubmit}>
-        {errorMsg ? <p style={{ color: 'red' }}>{errorMsg}</p> : null}
-        <label htmlFor="email">
-          <input
-            id="email"
-            type="email"
-            name="email"
-            placeholder="Email address"
-          />
-        </label>
-        <label htmlFor="password">
-          <input
-            id="password"
-            type="password"
-            name="password"
-            placeholder="Password"
-          />
-        </label>
+    <div>
+      <Footer />
+    </div>
+  )
+}
 
-        <hr />
-        
-        <button type="submit">Sign in</button>
-
-        <hr />
-
-        <Link href="/forgetpassword">
-          <a>Forget password</a>
-        </Link>
-
-      </form>
-    </>
-  );
-};
-
-export default LoginPage;
+export default login
---
+++ pages/pricing.js
@@ -0,0 +1,15 @@
+import PricingTiers from '../Components/pricingTiers/PricingTiers'
+import TopNav from '../Components/nav/TopNav'
+import Footer from '../Components/footer/Footer'
+
+const pricing = () => {
+    return (
+        <div>
+            <TopNav />
+            <PricingTiers />
+            <Footer />
+        </div>
+    )
+}
+
+export default pricing;
