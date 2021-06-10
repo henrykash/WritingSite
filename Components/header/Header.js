d5caaa8240faec148a45ff708d1fbba02212075d
nyambura00 <winnienyambura14@gmail.com>
2021-06-10 15:15:37 +0300

    Layout re-structured

--- Components/header/Header.js
+++ Components/header/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
                 <h2 class="title-font sm:text-4xl text-3xl mb-4 font-medium">Get Work Done <TypingComponent />
                   <br class="hidden lg:inline-block" />
                 </h2>
-                <p class="mb-8 leading-relaxed">Copper mug try-hard pitchfork pour-over freegan heirloom neutra air plant cold-pressed tacos poke beard tote bag. Heirloom echo park mlkshk tote bag selvage hot chicken authentic tumeric truffaut hexagon try-hard chambray.</p>
+                <p class="mb-8 leading-relaxed">Get your work done Effectively, Accurately, In-timely, Smartly and Affordably while you focus on other things!</p>
                 <div class="flex justify-center">
                   <Link href='/ordernow'>
                   <button class="inline-flex text-white bg-red-900 border-0 py-2 px-6 focus:outline-none hover:underline rounded text-lg">Order Now</button>
