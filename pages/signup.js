ee80a2331427af77f449885775d77340bb1530d9
nyambura00 <winnienyambura14@gmail.com>
2021-06-13 17:24:13 +0300

    Restructure SignUp page

--- pages/signup.js
+++ pages/signup.js
@@ -9,19 +9,23 @@ const Signup = () => {
   const onSubmit = (data) => {
     console.log(data);
   }
+
   return (
     <div>
-      <nav>
-        <Link href="/" className="inline-flex items-center">
+      <nav className="flex flex-row">
+        <Link href="/">
           <Image
-            className="ml-4"
+            className= "relative flex ml-2"
             src="/writing.png"
             alt="Premium logo"
             width={90}
             height={75}
           />
         </Link>
-        <div className="text-center block text-3xl bold">Gets Work Done is happy to see you here!</div>
+
+        <Link href="/login">
+        <div className="underline text-xl bold text-right absolute top-10 right-5" type="submit">LOG IN</div>
+        </Link>
       </nav>
       <main className="bg-gray-300">
         <style jsx>{`
@@ -47,37 +51,38 @@ const Signup = () => {
             input[type="submit"] {
               border: none;
               margin-top: 40px;
-              padding: 20px;
+              
             },
           `}
           </style>
 
         <form className="flex flex-col" onSubmit={handleSubmit(onSubmit)}> 
-          <div>
-            <label htmlFor="username">USERNAME: </label>
-            <input {...register("username")}/>
-          </div>
+          <p className="bold text-3xl">Create Your Account</p>
+            <div>
+              <label htmlFor="username">USERNAME: </label>
+              <input {...register("username")}/>
+            </div>
 
-          <div>
-            <label htmlFor="email">EMAIL ADDRESS: </label>
-            <input {...register("email address")} />
-          </div>
+            <div>
+              <label htmlFor="email">EMAIL ADDRESS: </label>
+              <input {...register("email address")} />
+            </div>
 
-          <div>
-            <label htmlFor="password">INPUT PASSWORD:</label>
-            <input {...register("password")} />
-          </div>
+            <div>
+              <label htmlFor="password">INPUT PASSWORD:</label>
+              <input {...register("password")} />
+            </div>
 
-          <div>
-            <label htmlFor="confirm-password">CONFIRM YOUR PASSWORD:</label>
-            <input {...register("confirm password")} />
-          </div>
+            <div>
+              <label htmlFor="confirm-password">CONFIRM YOUR PASSWORD:</label>
+              <input {...register("confirm password")} />
+            </div>
 
-          <div>
-            <Link href="/">
-              <button type="submit" className="bg-gray-500">CREATE ACCOUNT</button>
-            </Link>
-          </div>
+            <div>
+              <Link href="/">
+                <button type="submit" className="bg-gray-500 hover:underline">CREATE ACCOUNT</button>
+              </Link>
+            </div>
         </form>
       </main>
       <Footer />
