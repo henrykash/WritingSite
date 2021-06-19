1b1e90749f163fc127464a6b56ed05f2e23fb12d
nyambura00 <winnienyambura14@gmail.com>
2021-06-19 17:59:11 +0300

    configure PassportConfig file

--- node_app/server.js
+++ node_app/server.js
@@ -36,7 +36,20 @@ app.post('/login', (req, res)=> {
     console.log(req.body)
 })
 app.post('/register', (req, res)=> {
-    console.log(req.body)
+    User.findOne({ username: req.body.username }, async (err, doc) => {
+        if (err) throw err;
+        if (doc) res.send("User Already Exists");
+        if (!doc) {
+          const hashedPassword = await bcrypt.hash(req.body.password, 10);
+    
+          const newUser = new User({
+            username: req.body.username,
+            password: hashedPassword,
+          });
+          await newUser.save();
+          res.send("User Created");
+        }
+    });
 })
 app.get('/user', (req, res)=> {
     res.send(req.user);
