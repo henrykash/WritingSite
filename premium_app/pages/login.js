ea0df258f9a4008931662024cbbccd72402fb56c
nyambura00 <winnienyambura14@gmail.com>
2021-06-19 12:12:32 +0300

    set up login and signup pages

--- premium_app/pages/login.js
+++ premium_app/pages/login.js
@@ -1,6 +1,36 @@
+import {useState} from 'react';
+import axios from 'axios';
+
 const Login = () => {
+    const [loginUsername, setLoginUsername] = useState("");
+    const [loginPassword, setLoginPassword] = useState("");
+
+    const signin = () => {
+        axios({
+            method: 'post',
+            data: {
+                username: loginUsername,
+                password: loginPassword
+            },
+            withCredentials: true,
+            url: 'http://localhost:3000/login',
+        }).then((res) => console.log(res))
+    }
+
     return (
         <>
+        <div>
+            <h1>Login to exclusively enjoy our services</h1>
+                <input
+                placeholder="username"
+                onChange={(e) => setLoginUsername(e.target.value)}
+                />
+                <input
+                placeholder="password"
+                onChange={(e) => setLoginPassword(e.target.value)}
+                />
+            <button onClick={signin}>Submit</button>
+        </div>
         </>
     )
 }
--- premium_app/pages/signup.js
+++ premium_app/pages/signup.js
@@ -1,6 +1,38 @@
+ import {useState} from 'react';
+ import axios from 'axios';
+
 const Signup = () => {
+    const [registerUsername, setRegisterUsername] = useState("");
+    const [registerPassword, setRegisterPassword] = useState("");
+
+    const register = () => {
+        axios({
+            method: 'post',
+            data: {
+                username: registerUsername,
+                password: registerPassword
+            },
+            withCredentials: true,
+            url: 'http://localhost:3000/signup',
+        }).then((res) => console.log(res))
+    }
+    
     return (
         <>
+        <div>
+            <h1>Create an account to get started with us</h1>
+            <input
+            placeholder="username"
+            type="text"
+            onChange={(e) => setRegisterUsername(e.target.value)}
+            />
+            <input
+            placeholder="password"
+            type="password"
+            onChange={(e) => setRegisterPassword(e.target.value)}
+            />
+            <button onClick={register}>Submit</button>
+        </div>
         </>
     )
 }
